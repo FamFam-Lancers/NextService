aa6a0a63df9f5d68cc88c5c0a20c5776d36de37c
FamFam-Lancers <famfam.lancers@gmail.com>
2019-02-24 18:43:37 +0900

    確認用メール送信サービスを追加

---
+++ src/main/java/com/example/dbtest/domain/service/SendVerifyMailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.dbtest.domain.service;
+
+import com.example.dbtest.domain.entity.UserInfo;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class SendVerifyMailServiceImpl implements SendVerifyMailService {
+    private final JavaMailSender mailSender;
+
+    public SendVerifyMailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void execute(UserInfo userInfo) {
+        mailSender.send(mimeMessage -> {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, StandardCharsets.UTF_8.name());
+            helper.setFrom("XXX Symtem <info@hoge.info>");
+            helper.setTo(userInfo.getEmail());
+            helper.setSubject("Please verify your email.");
+
+            String text = "<html><body><div>Hi " + userInfo.getUsername() + ", welcome to XXX System!</div>" +
+                    "<div><a href='http://localhost:8080/register/verify?id=" + userInfo.getTempkey() + "'>Verify</a></div>" +
+                    "</body></html>";
+
+            helper.setText(text, true);
+        });
+    }
+
+}
